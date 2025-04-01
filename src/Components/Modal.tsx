e3b9cc8fb35ccf85ca3f6af19c06da5218390701
renys <renys_northside@mail.ru>
2025-04-01 13:38:39 +0500

    new json with quests

--- src/Components/Modal.tsx
+++ src/Components/Modal.tsx
@@ -6,7 +6,10 @@ function Modal({ isOpen }: { isOpen: { data: boolean } }) {
     return (
         <div>
             <div className='fixed w-full h-full bg-black opacity-45 z-10'></div>
-            <div className='fixed z-20 bg-white top-[15%] left-[50%] translate-x-[-50%]  max-w-[600px] mx-auto p-[30px] rounded-2xl '>
+            <div className='fixed z-20 bg-white top-[15%] left-[50%] translate-x-[-50%]  max-w-[600px] mx-auto p-[30px] rounded-2xl text-base/relaxed '>
+                <p className='text-center text-[22px] mb-[15px] '>
+                    Анкета удовлетворенности пациента качеством оказанных услуг{" "}
+                </p>
                 <p className='mb-[30px]'>
                     Уважаемый пациент! В целях совершенствования качества услуг
                     руководство клиники просит заполнить анкету. Анонимность
@@ -20,7 +23,7 @@ function Modal({ isOpen }: { isOpen: { data: boolean } }) {
                 <div className='flex justify-center'>
                     <button
                         onClick={() => isOpen(false)}
-                        className=' cursor-pointer bg-[#1BB394] text-white py-[10px] px-[25px] rounded-xl'>
+                        className=' cursor-pointer bg-[#1BB394] text-white py-[8px] px-[25px] rounded-xl'>
                         Согласен
                     </button>
                 </div>
